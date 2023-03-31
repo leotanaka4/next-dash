1e4cda50d100159498ddc545b5baca769a941b6d
Leonardo Tanaka <89543577+leotanaka4@users.noreply.github.com>
2023-03-31 18:19:40 -0300

    Fim do vídeo

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import '@/styles/globals.css'
+import Sidebar from '../components/Sidebar'
+
+export default function App({ Component, pageProps }) {
+  return (
+  <Sidebar>
+    <Component {...pageProps} />
+  </Sidebar>)
+}
